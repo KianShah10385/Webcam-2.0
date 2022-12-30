fae45665b6f0cf60e30af6b19fe036bbf8227499
KianShah10385 <108907854+KianShah10385@users.noreply.github.com>
2022-12-29 20:48:25 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ camera = document.getElementById("camera");
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_img" src="'+data_uri;+'">';
+        document.getElementById("result").innerHTML = '<img id="selfie_img" src="'+data_uri+'">';
     });
 }
 function save()
